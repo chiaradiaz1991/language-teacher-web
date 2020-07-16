5f87ca430dbbdec85b3ef042c9c8cc80b5fc4820
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2020-07-16 13:12:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <LocaleSwitcher />
+    <p>{{ $t('main.welcome', { company: 'Lokalise'}) }}</p>
+  </div>
+</template>
+
+<script>
+import LocaleSwitcher from './components/LocaleSwitcher'
+export default {
+  name: 'App',
+  components: {
+    LocaleSwitcher
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'pl',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}
